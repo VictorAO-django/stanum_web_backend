506ecab26b2cc49749af3c77524f5795c4298a6f
Olusola Ayomide <Ayomideolusola5002@gmail.com>
2025-07-27 07:22:28 +0100

    fixng asgi deploy error

--- stanum_web/asgi.py
+++ stanum_web/asgi.py
@@ -7,18 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-import os
-
+import os, django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stanum_web.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from utils.middleware import TokenAuthMiddlewareStack
-from stanum_socket.routing import *
+from stanum_socket.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
