6aee96570ec89835fcb181cb972319c46955e854
Olusola Ayomide <Ayomideolusola5002@gmail.com>
2025-08-19 19:15:14 +0100

    fixing error

--- stanum_web/settings.py
+++ stanum_web/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = [
-    "https://stanumcapital.com",
+    "https://stanumcapital.com"
 ]
 
 AUTH_USER_MODEL = "account.User"
